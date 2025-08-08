1a2ad889ea08840cb1afd36702fed4f5e2318d04
Evgeniia Kharitonova <kuznetsovaevgeni@gmail.com>
2025-08-08 21:46:03 +0300

    first commit

---
+++ src/test/java/VkusvillTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.MainPage;
+
+public class VkusvillTests extends TestBase {
+    MainPage mainPage = new MainPage();
+
+    @Test
+    @Tag("Promo")
+    void openPageTest() {
+
+    }
+}
